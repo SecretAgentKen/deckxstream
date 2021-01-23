458a4dd22eb982ed01ca543b20a19b88fca36c74
Ken VanderVeer <gtaluvit@gmail.com>
2021-01-23 14:03:19 -0500

    Deck Manager tests doneish

--- test/testDeckManager.js
+++ test/testDeckManager.js
@@ -1,10 +1,12 @@
+const rewire = require('rewire');
+const events = require('events');
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
 const sc = require('sinon-chai');
 chai.use(sc);
 
-const deckManager = require('../lib/deckManager');
+const deckManager = rewire('../lib/deckManager');
 
 const PIXEL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2NYlV/1HwAF7QKTgvPu3QAAAABJRU5ErkJggg==";
 
@@ -13,6 +15,9 @@ describe('Deck Manager', function() {
 	beforeEach(function() {
 		deck = {clearAllKeys: sinon.fake(), clearKey: sinon.fake(), ICON_SIZE: 32, KEY_COLUMNS: 2, KEY_ROWS: 2, fillPanel: sinon.fake(), setBrightness: sinon.fake()};
 		buttons = new Array(6).fill(0).map(()=>{return {stop: sinon.spy(), start: sinon.spy()};});
+	});
+	afterEach(function() {
+		sinon.restore();
 	});
 
 	describe('Constructor',function() {
@@ -37,7 +42,6 @@ describe('Deck Manager', function() {
 			dm = new deckManager(deck, buttons, {screensaver:{animation: require('path').join(__dirname,'../test_resources/blink.gif'), brightness: 10}});
 		});
 		afterEach(function() {
-			sinon.restore();
 			dm.stopScreensaver();
 		});
 		it('should stop all buttons when screensaver started',function() {
@@ -72,9 +76,7 @@ describe('Deck Manager', function() {
 		beforeEach(function() {
 			dm = new deckManager(deck, buttons, {sticky:[{keyIndex:0, icon: PIXEL}]});
 		});
-		afterEach(function() {
-			sinon.restore();
-		});
+
 		it('should start sticky buttons on startup', function(){
 			let spy = sinon.spy(buttons[0], 'start');
 			return buttons[0].isReady.then(()=>{
@@ -103,9 +105,7 @@ describe('Deck Manager', function() {
 				buttons:[{keyIndex:0, icon: PIXEL}]
 			}]});
 		});
-		afterEach(function() {
-			sinon.restore();
-		});
+
 		it('should load a button on the proper page', function(){
 			expect(buttons[0].render).to.be.undefined;
 			dm.changePage('testPage');
@@ -116,23 +116,86 @@ describe('Deck Manager', function() {
 	});
 
 	describe('Dynamic page', function(){
-		let dm, spawn;
+		let dm, spawn, revert, so;
 		beforeEach(function() {
-			dm = new deckManager(deck, buttons, {pages:[{
-				pageName:'testPage',
-				dynamicPage: 'testing'
-			}]});
+			so = {
+				stdout: new events.EventEmitter()
+			};
+			spawn = sinon.stub().returns(so);
+			revert = deckManager.__set__('spawn',spawn);
+			dm = new deckManager(deck, buttons, {
+				pages:[{
+					pageName:'testPage',
+					dynamicPage: 'testing'
+				}]
+			});
 		});
 		afterEach(function() {
-			sinon.restore();
+			revert();
 		});
+
 		it('should call the command specified on page load', function(){
-			spawn = sinon.fake();
-			dm.spawn = spawn;
-			spawn.stdout = {on: sinon.fake()};
 			expect(spawn).to.not.be.called;
 			dm.changePage('testPage');
 			expect(spawn).to.be.calledOnce;
 		});
+		it('should load a page based on command output', function(){
+			dm.changePage('testPage');
+			buttons.forEach((b)=>{
+				expect(b).to.be.null;
+			})
+			so.stdout.emit('data', JSON.stringify({
+				buttons: [
+					{
+						keyIndex: 1,
+						icon: PIXEL
+					},
+					{
+						keyIndex: 3,
+						icon: PIXEL
+					}
+				]
+			}));
+			buttons.forEach((b,i)=>{
+				if ( i === 1 || i === 3 ) {
+					expect(b).to.not.be.null;
+				} else {
+					expect(b).to.be.null;
+				}
+			})
+		});
+		it('should survive bad JSON', function(){
+			let con = sinon.stub(console, 'error');
+			dm.changePage('testPage');
+			buttons.forEach((b)=>{
+				expect(b).to.be.null;
+			})
+			expect(con).to.not.be.called;
+			so.stdout.emit('data', 'garbage');
+			buttons.forEach((b)=>{
+				expect(b).to.be.null;
+			})
+			expect(con).to.be.calledOnce;
+		})
+	});
+
+	describe('setBrightness', function(){
+		let dm;
+		beforeEach(function() {
+			dm = new deckManager(deck, buttons, {brightness: 15});
+		});
+
+		it('should store the value to allow restoration', function(){
+			dm.setBrightness(50);
+			expect(dm.storedBrightness).to.eql(50);
+			dm.setBrightness(5);
+			expect(dm.storedBrightness).to.eql(5);
+		})
+
+		it('should set brightness on the deck', function(){
+			expect(deck.setBrightness).to.not.be.called;
+			dm.setBrightness(5);
+			expect(deck.setBrightness).to.be.calledOnce;
+		})
 	})
 });
